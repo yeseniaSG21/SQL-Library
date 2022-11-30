4a35b467cc0727f5e66ceccf585c42cbd7f25ff6
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-29 18:59:09 -0800

    Update

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Import Sequelize
+const { Sequelize } = require('sequelize');
+
+//Connect to the database and sync the model 
+
+
+
+
 //View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
