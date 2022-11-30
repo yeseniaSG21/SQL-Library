f568415bc06388d72c3c5f6c76457f171f9cd8c5
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-29 17:44:36 -0800

    Add express-generator

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-//Import the instance of sequelize
-const { sequelize } = require('./models');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-//Asynchronously connect to the database
-sequelize.authenticate()
-    .then( () => {
-        console.log('Connection to the database successful');
-        return sequelize.sync();
-    })
-    .then( () => {
-        console.log('Synchronizing successful')
-    })
-    .catch( (error) => {
-        console.log('Error connecting to the database: ', error);
-    })
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
