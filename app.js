d5f5a4261ed36465ea66f38175b15ba3dee03bdf
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-30 17:53:02 -0800

    Update

--- app.js
+++ app.js
@@ -4,48 +4,57 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-//Import Sequelize
-const sequelize = require('./models/index').sequelize;
+//Import Routes Paths
+var indexRouter = require('./routes/index');
 
-//Sync the model 
-(async () => {
-    sequelize.sync()
-    .then (() => console.log('Connection to the database successful'));
-})();
+//Import Sequelize
+const { sequelize } = require('./models');
+
+//Test connection to database and sync the model
+sequelize.authenticate()
+    .then(() => {
+        console.log("Connection to the database successful!")
+        return sequelize.sync()
+    })
+    .then(() => {
+        console.log("Model successfully sync with database")
+    })
+    .catch( error => {
+        console.log("Error connecting to the database: ", error)
+    })
 
 //View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
+//Add static middleware
+app.use('/static', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//Use Routes Path
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+//Catch 404 and forward to error handler
+app.get('*', (req, res, next) => {
+    next(createError(404), "Unfortunately the page you are looking for is not found. Please return to the homepage.");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+//Error handler
+app.use((error, req, res, next) => {
+    if (error.status === 404) {
+        res.status(404).render('page-not-found', { error, title: 'Page Not Found!' });
+        console.log('404 Error Occured!');
+    } else {
+        err.message = err.message || "Uh no! Looks like something went wrong on the server. Return to Homepage.";
+        res.status(err.status || 500).render('error', { error, title: 'Server Error!' });
+    }
 });
 
 module.exports = app;
