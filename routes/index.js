864be58f5e8ec856ce432ca7bfeaefd3ec6f112a
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-30 16:49:21 -0800

    Update

--- routes/index.js
+++ routes/index.js
@@ -17,32 +17,37 @@ function asyncHandler(cb) {
   }
 }
 
-/* GET home page */
-router.get('/', asyncHandler(async (req, res) => {
-  const books = await Book.findAll({ order: [["createdAt", "DESC"]] });
-  res.render("books/index", { books, title: "Library Books" });
-}));
-
 /* Home route should redirect to the /books route */
 router.get('/', asyncHandler(async (req, res) => {
-  res.redirect('books')
+  res.redirect('/books')
 }));
 
 /* Shows the full list of books */
-router.get('/books', )
-
+router.get('/books', asyncHandler(async (req, res) => {
+  const books = await Book.findAll({ order: [["createdAt", "DESC"]] });
+  res.render("index", { books, title: "Library Books" });
+}));
 
 /* Shows the create new book form */
-
-
-
-
+router.get('/books/new', (req, res) => {
+  res.render("books/new-book", { book: {}, title: "Add New Book" });
+});
 
 /* Posts a new book to the database */
-
-
-
-
+router.post('/books/new', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books/" + book.id);
+  } catch (error) {
+    if (error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      res.render("books/new-book", { book, errors: error.errors, title: "Add New Book" })
+    } else {
+      throw error;
+    }  
+  }
+}));
 
 /* Shows book detail form */
 router.get('/books/:id', asyncHandler(async (req, res) => {
