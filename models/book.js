de2a57568ddb7b8163d9efdbce64fd3a3dcf8ab5
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-30 21:15:28 -0800

    Update

--- models/book.js
+++ models/book.js
@@ -6,6 +6,11 @@ const Sequelize = require('sequelize');
 module.exports = ( sequelize ) => {
   class Book extends Sequelize.Model {}
   Book.init({
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     title: {
       type: Sequelize.STRING,
       allowNull: false,
